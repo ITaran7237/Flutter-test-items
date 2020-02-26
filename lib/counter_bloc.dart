06bca5ab53dada4fb98a8199fe9373e122295ed7
Igor Taran <igor.taran@chisw.com>
2020-02-26 16:39:15 +0200

    added test_ex_6

---
+++ lib/counter_bloc.dart
@@ -0,0 +1,33 @@
+import 'dart:async';
+
+import 'package:first_flutter_app/counter_event.dart';
+
+class CounterBloc {
+  int _counter = 0;
+
+  final _counterStateController = StreamController<int>();
+  final _eventStateController = StreamController<CounterEvent>();
+
+  CounterBloc() {
+    _eventStateController.stream.listen(_mapEventToState);
+  }
+
+  StreamSink<int> get _inCounter => _counterStateController.sink;
+  //output stream
+  Stream<int> get counter => _counterStateController.stream;
+  //input stream
+  Sink<CounterEvent> get counterEventSink => _eventStateController.sink;
+
+  void _mapEventToState(CounterEvent event) {
+    if (event is IncrementEvent)
+      _counter++;
+    else
+      _counter--;
+    _inCounter.add(_counter);
+  }
+
+  void dispose() {
+    _counterStateController.close();
+    _eventStateController.close();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
-import 'package:first_flutter_app/LessonDetails.dart';
+import 'package:first_flutter_app/counter_event.dart';
 import 'package:flutter/material.dart';
+
+import 'counter_bloc.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,7 +10,58 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(body: LessonDetails()),
+      home: CounterWidget(),
     );
+  }
+}
+
+class CounterWidget extends StatefulWidget {
+  @override
+  _CounterWidgetState createState() => _CounterWidgetState();
+}
+
+class _CounterWidgetState extends State<CounterWidget> {
+  CounterBloc counterBloc = CounterBloc();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text('You have pushed the button this many times:'),
+              StreamBuilder<int>(
+                  stream: counterBloc.counter,
+                  initialData: 0,
+                  builder: (context, snapshot) {
+                    return Text(
+                      '${snapshot.data.toString()}',
+                      style: Theme.of(context).textTheme.headline2,
+                    );
+                  }),
+            ],
+          ),
+        ),
+        floatingActionButton: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            FloatingActionButton(
+              onPressed: () => counterBloc.counterEventSink.add(IncrementEvent()),
+              child: Icon(Icons.add),
+            ),
+            SizedBox(width: 16.0),
+            FloatingActionButton(
+              onPressed: () => counterBloc.counterEventSink.add(DecrementEvent()),
+              child: Icon(Icons.remove),
+            )
+          ],
+        ));
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    counterBloc.dispose();
   }
 }
